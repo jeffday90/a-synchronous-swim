7ce244b31ec307e133548c22c96733f5825f6784
jeffday90 <jeffrey.day00@gmail.com>
2019-04-27 16:40:45 -0700

    Successfully connected server and client.

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -11,13 +11,33 @@ module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 
 module.exports.router = (req, res, next = () => { }) => {
   // console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
   if (req.method === 'GET') {
-    //dequere from the queue and return to client
+    // if (req.is('text')) {
+    //   console.log('Successful type pass')
+    // }
+    console.log(req.headers);
+    res.writeHead(200, headers);
     var commands = ['left', 'right', 'up', 'down'];
     var index = Math.round(Math.random() * 3);
     res.end(messages.dequeue());
   } else {
     res.end();
   }
+
+
+
+
+  // if (req.method === 'POST') {
+  //   console.log('POST made it to server!!!');
+  //   res.end()
+
+  //we need to figure out whether the picture (from the client)
+  //exists within our server folder
+  //how do we capture the piece of data from the client to determine
+  //whether exists
+  //chunk
+
+  //if picture doesn't exist
+  //return 404 -> to the client or just to the server?
+  // }
 };
